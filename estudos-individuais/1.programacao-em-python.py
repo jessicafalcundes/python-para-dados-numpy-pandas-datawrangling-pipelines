15ffa664715a4e7bacb8aed2c5be651d0780d177
jessicafalcundes <jessicafalcundes@outlook.com>
2025-03-08 02:39:08 -0300

    anotações

--- estudos-individuais/1.programacao-em-python.py
+++ estudos-individuais/1.programacao-em-python.py
@@ -857,3 +857,68 @@ print(nova_lista)'''  # [1, 2, 3]
 indice = lista.index(3)  # Retorna o índice da primeira ocorrência do número 3
 print(indice)'''  # 2
 
+# Manipulando dados dentro das Lista
+Lista_Vazia = []
+'''print('Lista antes:', Lista_Vazia, '\n' )'''
+
+# Inserindo valores na Lista
+'''Lista_Vazia.append(1)
+Lista_Vazia.append(2)
+Lista_Vazia.append(3)
+print('Lista Depois:', Lista_Vazia, '\n' )'''
+
+# Tamanho da lista
+'''print('Lista contém:', len(Lista_Vazia), 'elementos', '\n' )'''
+
+# Tamanho da lista
+'''print('Acessando o 1º Elemento:', Lista_Vazia[0] )
+print('Acessando o 2º Elemento:', Lista_Vazia[1] )
+print('Acessando o último Elemento:', Lista_Vazia[-1] )
+print('Acessando um periodo:', Lista_Vazia[0:2], '\n' )'''
+
+# Excluindo valor na lista
+'''del Lista_Vazia[1]
+print('Depois da exclusão:', Lista_Vazia, '\n' )'''
+
+# Limpando a lista
+'''print('Depois da Limpeza:', Lista_Vazia.clear(), '\n' )'''
+
+# Inserindo um valor com uma posição
+'''Lista_Inserir = ['Posição 1', 'Posição 2', 'Posição 3']
+Lista_Inserir.insert( 0, 'Posição 4' )
+print( Lista_Inserir, '\n' )'''
+
+# Inserindo uma lista na outra
+'''Lista_Inserir_01 = ['Posição 1', 'Posição 2', 'Posição 3']
+Lista_Inserir_02 = ['Posição 4', 'Posição 5', 'Posição 6']
+Lista_Inserir_01.extend( Lista_Inserir_02 )
+print( Lista_Inserir_01, '\n' )'''
+
+# Removendo itens especifico pelo nome
+'''Lista_Inserir_01.remove('Posição 6')
+print( Lista_Inserir_01, '\n' )'''
+
+# Removendo itens pelo index
+'''Lista_Inserir_01.pop(0)
+print( Lista_Inserir_01, '\n' )'''
+
+# Ordenar uma lista
+'''Lista_Alfabetica = ['Z', 'C', 'A', 'B', 'L']
+Lista_Alfabetica.sort()
+print( Lista_Alfabetica, '\n' )'''
+
+# Ordenar uma lista de forma inversa
+'''Lista_Alfabetica.sort( reverse=True )
+print( Lista_Alfabetica, '\n' )'''
+
+# Copiar uma Lista
+'''Lista_Alfabetica_02 = Lista_Alfabetica.copy()
+print( Lista_Alfabetica_02, '\n' )'''
+
+# Indetificar o Index do elemento na lista
+'''print( Lista_Alfabetica_02.index('L'), '\n' )'''
+
+
+
+# pacote datetime
+
